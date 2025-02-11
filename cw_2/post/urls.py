7fa9aaf10ef698228758972e413accf642b81b00
Papa <askartursymbay@gmail.com>
2025-02-11 14:10:52 +0500

    first commit

---
+++ cw_2/post/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.post_list,name='post_list'),
+    path('<int:id>/',views.post_detail,name='post_detail'),
+    path('new/',views.post_create,name='post_create'),
+    path('<int:id>/delete/',views.post_delete,name='post_delete')
+]
